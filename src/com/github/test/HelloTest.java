742b2c6cacd158f167e07923feb41983b256451a
CNazp <cn_hlj_azp@sina.com>
2016-12-22 14:31:24 +0800

    Update HelloTest.java

--- src/com/github/test/HelloTest.java
+++ src/com/github/test/HelloTest.java
@@ -3,6 +3,7 @@ package com.github.test;
 public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("111111"+"一一一一一");
+		System.out.println("222222"+"二二二二二");
 	}
 
 }
